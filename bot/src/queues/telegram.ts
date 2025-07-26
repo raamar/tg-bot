a665641f69fd9339aa2a7bcd67ffe693f63c9188
Romil <khandryga.romil@gmail.com>
2025-07-26 18:14:00 +0300

    chore: initial update

--- bot/src/queues/telegram.ts
+++ bot/src/queues/telegram.ts
@@ -6,11 +6,6 @@ new Worker(
   'telegram',
   async (job) => {
     console.log(`Added massage `)
-    await prisma.message.create({
-      data: {
-        message: job.data.message,
-      },
-    })
     // const payload = job.data
 
     // await redis.rpush('telegram:buffer', JSON.stringify(payload))
