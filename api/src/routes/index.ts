55db18cb9b7447532ca68c2519c6d786209f3814
Romil <khandryga.romil@gmail.com>
2025-07-27 14:56:30 +0300

    feat: telegram endpoint security update

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -2,12 +2,23 @@ import { Router } from 'express'
 import { telegramQueue } from '../queues/telegram'
 
 const router = Router()
+const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN
+
+if (!process.env.TELEGRAM_TOKEN) {
+  throw new Error('TELEGRAM_TOKEN is not defined')
+}
 
 router.get('/ping', (req, res) => {
   res.status(200).send('pong')
 })
 
-router.post('/telegram-webhook', async (req, res) => {
+router.post('/telegram-webhook/:token', async (req, res) => {
+  const tokenFromUrl = req.params.token
+
+  if (tokenFromUrl !== TELEGRAM_TOKEN) {
+    return res.status(403).send('Invalid token')
+  }
+
   const update = req.body
 
   await telegramQueue.add('process-update', update)
