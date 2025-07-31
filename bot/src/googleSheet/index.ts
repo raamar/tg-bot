f4aac044532f512ded61151b36162fb869acea3e
Romil <khandryga.romil@gmail.com>
2025-07-31 08:45:52 +0300

    fix: add processing key

--- bot/src/googleSheet/index.ts
+++ bot/src/googleSheet/index.ts
@@ -12,7 +12,6 @@ if (!process.env.GOOGLE_SHEET_ENDPOINT) {
 }
 
 const FLUSH_INTERVAL_MS = parseInt(process.env.GOOGLE_SHEET_INTERVAL, 10)
-const REDIS_KEY = 'sheets:buffer'
 const SHEETS_ENDPOINT = process.env.GOOGLE_SHEET_ENDPOINT
 
 export const googleSheetQueue = new Queue<SheetLog>('googleSheet', {
@@ -25,71 +24,39 @@ export const googleSheetQueue = new Queue<SheetLog>('googleSheet', {
   },
 })
 
-const startSheetsManager = () => {
-  setInterval(async () => {
-    const count = await redis.llen(REDIS_KEY)
-    if (count === 0) {
-      // console.log('⏳  Google Sheet: buffer is empty, skipping...')
-      return
+const FLUSH_BUFFER_KEY = 'sheets:buffer'
+const PROCESSING_BUFFER_KEY = 'sheets:processing'
+
+setInterval(async () => {
+  const bufferExists = await redis.exists(FLUSH_BUFFER_KEY)
+  if (!bufferExists) return
+
+  const count = await redis.llen(FLUSH_BUFFER_KEY)
+  if (count === 0) return
+
+  const renamed = await redis.rename(FLUSH_BUFFER_KEY, PROCESSING_BUFFER_KEY).catch((err) => {
+    if (err.message.includes('no such key')) {
+      return false
     }
-
-    const items = await redis.lrange(REDIS_KEY, 0, -1)
-    const parsed: SheetLog[] = items.map((item) => JSON.parse(item))
-
-    try {
-      await axios.post(SHEETS_ENDPOINT, parsed)
-      await redis.del(REDIS_KEY)
-      console.log('✅ Google Sheet: log sended.')
-    } catch (err) {
-      console.error('❌ Google Sheet: Failed to send data to Sheets:', err)
-    }
-  }, FLUSH_INTERVAL_MS)
-
-  new Worker<SheetLog>(
-    'googleSheet',
-    async (job) => {
-      const payload = job.data
-
-      if (!payload.user_id) {
-        console.warn('⚠️ Google Sheet: Invalid log entry: missing user_id')
-        return
-      }
-
-      const sortedPayload: Record<string, unknown> = Object.keys(payload)
-        .sort()
-        .reduce((acc, key) => {
-          acc[key] = payload[key as keyof SheetLog]
-          return acc
-        }, {} as Record<string, unknown>)
-
-      await redis.rpush(REDIS_KEY, JSON.stringify(sortedPayload))
-      // const count = await redis.llen(REDIS_KEY)
-      // console.log(`🗂️  Google Sheet: Sheets log added. Current buffer size: ${count}`)
-    },
-    {
-      connection: redis,
-    }
-  )
-}
-
-/**
- * FIRST INIT FOR SHEETS
- */
-googleSheetQueue
-  .add('update', {
-    user_telegram_id: 'SYSTEM',
-    user_id: 'RESTART',
-    username: '',
-    first_name: '',
-    last_name: '',
-    joined_at: '',
-    ref_code: '',
-    stage: '',
-    amount: '',
-    order_url: '',
-    paid_at: '',
-    payment_status: '',
+    throw err
   })
-  .then(() => {})
 
-startSheetsManager()
+  if (renamed === false) return
+
+  const items = await redis.lrange(PROCESSING_BUFFER_KEY, 0, -1)
+  const parsed: SheetLog[] = items.map((item) => JSON.parse(item))
+
+  try {
+    await axios.post(SHEETS_ENDPOINT, parsed)
+    await redis.del(PROCESSING_BUFFER_KEY)
+    console.log('✅ Google Sheet: log sent.')
+  } catch (err) {
+    console.error('❌ Google Sheet: Failed to send data to Sheets:', err)
+
+    if (parsed.length > 0) {
+      const values = parsed.map((item) => JSON.stringify(item))
+      await redis.lpush(FLUSH_BUFFER_KEY, ...values.reverse())
+    }
+    await redis.del(PROCESSING_BUFFER_KEY)
+  }
+}, FLUSH_INTERVAL_MS)
