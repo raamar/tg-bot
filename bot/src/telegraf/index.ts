a90bbe8631065aaca9735109ad271af43355bdad
Romil <khandryga.romil@gmail.com>
2026-02-27 23:23:01 +0300

    fix: ref

--- bot/src/telegraf/index.ts
+++ bot/src/telegraf/index.ts
@@ -216,7 +216,6 @@ bot.start(
             username,
             firstName: first_name,
             lastName: last_name,
-            refSource: ref,
             blockedByUser: false,
             blockedAt: null,
             blockReason: null,
